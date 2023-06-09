8681118ca90c3567ef3ebd0ecdbc0a49e5115cc3
Hossein Moosavi Ph.D <h.moosavi57@gmail.com>
2023-06-10 00:19:18 +0200

    Update section1.js

--- section1.js
+++ section1.js
@@ -6,7 +6,7 @@ console.clear();
 let scene = new THREE.Scene();
 scene.background = new THREE.Color(0x160016);
 let camera = new THREE.PerspectiveCamera(60, innerWidth / innerHeight, 1, 1000);
-camera.position.set(0, 10,30);
+camera.position.set(0, 19,30);
 let renderer = new THREE.WebGLRenderer();
 renderer.setSize(innerWidth, innerHeight);
 document.body.appendChild(renderer.domElement);
